38c9908fd3dff946731f19a2198e44d21966b983
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-05-22 21:57:39 +0300

    Added permission handling

--- commands/Stunda.js
+++ commands/Stunda.js
@@ -1,14 +1,20 @@
 const {SlashCommandBuilder} = require('@discordjs/builders')
+const {Permissions} = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder().setName('stunda').setDescription('Izveido pierakstu kanālu mācību stundai').addStringOption(option => option.setName('nosaukums').setDescription('Ievadiet pierakstu kanāla nosaukumu')),
     async execute(interaction){
-        const thread = await interaction.channel.threads.create({
-            name: interaction.options.getString('nosaukums'),
-            autoArchiveDuration: 60,
-            reason: 'Pierakstu kanāls mācību stundai'
-        })
+        if(interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)){
+            const thread = await interaction.channel.threads.create({
+                name: interaction.options.getString('nosaukums'),
+                autoArchiveDuration: 60,
+                reason: 'Pierakstu kanāls mācību stundai'
+            })
 
-        await interaction.reply('Kanāls pierakstiem izveidots veiksmīgi!')
+            await interaction.reply('Kanāls pierakstiem izveidots veiksmīgi!')
+        }else{
+            interaction.reply('Tev nav atļaujas izmantot šo komandu!')
+        }
+
     }
 }
--- commands/Tezaurs.js
+++
@@ -1,9 +0,0 @@
-const {SlashCommandBuilder} = require('@discordjs/builders')
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('tezaurs').setDescription('Komanda, lai iegūtu vārda skaidrojumu, izmantojot Tezaurs.lv API')
-        .addStringOption(option => option.setName('vārds').setDescription('Ievadi vārdu, kuram vēlies skaidrojumu')),
-    async execute(interaction){
-        interaction.reply("Pie tā vēl strādāju")
-    }
-}
