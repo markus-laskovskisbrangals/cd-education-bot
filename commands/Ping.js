e2c99ccd8fa63492cd3ff517fa50c1432747c017
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-05-23 14:07:58 +0300

    Added comments for Ping

--- commands/Ping.js
+++ commands/Ping.js
@@ -1,8 +1,14 @@
+//Koda fails /ping komandai
+
+//Moduļa importēšana
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
+//Jauna komandas moduļa izveide
 module.exports = {
+    //Jauna komandas objekta izveide
     data: new SlashCommandBuilder().setName('ping').setDescription('Komanda, lai pārliecinātos par bota darbību'),
     async execute(interaction){
+        //Ja Komanda nostrādā un bots ir uzstāts, čatā tiek nosūtīta ziņa
         await interaction.reply('Skolas asistents ir ieslēgts un gatavs darbam.')
     }
 }
