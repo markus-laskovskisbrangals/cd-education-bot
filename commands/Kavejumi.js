78d5abd62f01d151ea618245f60038fdb8fbc64e
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-05-23 14:00:45 +0300

    Added comments for kavejumi

--- commands/Kavejumi.js
+++ commands/Kavejumi.js
@@ -1,36 +1,44 @@
+//Koda fails /kavejumi komandai
+
+//Bibliotēku un moduļu importēšana
 const {SlashCommandBuilder} = require('@discordjs/builders')
 const {Permissions, MessageEmbed} = require('discord.js')
 const sqlite3 = require('sqlite3').verbose()
 const database = new sqlite3.Database('./database/discordbot.db')
 
-
+//Jauna komandas moduļa izveide
 module.exports = {
+    //Jauna komandas objekta izveide
     data: new SlashCommandBuilder().setName('kavejumi').setDescription('komanda skolotājam, lai apskatītu skolēnu sarakstu, kas ir bijuši stundā'),
     async execute(interaction){
+        //Datuma iegūšana
         const time = Date.now()
         const dateTime = new Date(time)
         const year = dateTime.getFullYear()
         const month = dateTime.getMonth() + 1
         const day = dateTime.getDate()
         let date = ''
+        //Ja mēnesis ir mazāks par oktobri, tad tam priekš tiek pievienota 0
         month < 10 ? date =  year + '-' + '0' + month + '-' + day : date = year + '-' + month + '-' + day
-        let attendants = []
+        //Jauna embed objekta izveide skolēnu saraksta attēlošanai
         const resultEmbed = new MessageEmbed()
             .setTitle(`Stundas šodien (\`${date}\`) apmeklēja:`)
             .setColor('#7ee500')
+        //Ja lietotājam ir administratora tiesības, tad notiek komandas izpilde
         if(interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)){
+            //Tiek nosūtīts SELECT pieprasījums datubāzei
             database.all(`SELECT * FROM attendance WHERE registration_date LIKE '%${date}%'`, (err, rows) => {
                 if(err){
+                    //Ja rodas problēma ar datubāzi, tiek izvadīts kļūdas paziņojums
                     interaction.reply('Iegūstot datus no datubāzes, radās problēma!')
+                    return
                 }
-                console.log(`Rowa ${rows.length}`)
+                //Cikls, kas katru no datubāzes iegūto skolēnu pievieno Embed objektam
                 rows.forEach(row => {
                     const userName = row.user_name
-                    console.log(`${userName} - ${row.registration_date}`)
-                    attendants.push(userName)
                     resultEmbed.addField(`${userName}`, 'Reģistrēts')
                 })
-                console.log(`Attendants ${attendants.length}`)
+                //Čatā tiek nosūtīts saraksta ar skolēniem, kas apmeklējuši stundu
                 interaction.reply({embeds: [resultEmbed]})
             })
         }else{
